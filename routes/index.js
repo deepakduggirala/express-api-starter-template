ad2c1fa48c63d1593ced1fcf126a40954486d8b9
Deepak Duggirala <ddeepak6992@gmail.com>
2023-01-24 17:56:57 -0500

    add health URL

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/health', (req, res) => {
+  res.send('OK');
+});
+
 router.use('/users', require('./users'));
 
 module.exports = router;
